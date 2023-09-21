c57a80d8fa7dab11b33ae74547953fc75f4d4c91
A l f i n d o <75003444+alfinkresna@users.noreply.github.com>
2023-09-21 13:15:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,106 +1,85 @@
-import source
-import hashlib, os, sys
+from source import Menu
+import hashlib, os
 
 
-class Sys_tm:
+class SystemSet:
 	
-	def __init__(self, menu):
+	def __init__(self, menu) -> None:
 		self.menu = menu
-		
-									
+											
 	def clear(self):
-	  if os.name.__eq__("posix"):
-	    os.system("clear")
+	  if os.name == "posix":
+	  	os.system("clear")
 	  else:
 	  	os.system("cls")
 	
 
-class Data:
+class GetHash:
 	
 	def sha256(self):
 		message = input("Input Pesan : ").encode()
 		return f"\nSHA-256 : {hashlib.sha256(message).hexdigest()}"
-
 		
 	def sha384(self):
 		message = input("Input Pesan : ").encode()
-		return f"\nSHA-384 : {hashlib.sha384(message).hexdigest()}"
-		
+		return f"\nSHA-384 : {hashlib.sha384(message).hexdigest()}"		
 		
 	def sha512(self):
 		message = input("Input Pesan : ").encode()
-		return f"\nSHA-512 : {hashlib.sha512(message).hexdigest()}"
+		return f"\nSHA-512 : {hashlib.sha512(message).hexdigest()}"		
 		
-		
-	def sha3_256(self) -> str:
+	def sha3_256(self):
 		message = input("Input Pesan : ").encode()
-		print(f"\nSHA3-256 : {hashlib.sha3_256(message).hexdigest()}")
-		
-		
+		return f"\nSHA3-256 : {hashlib.sha3_256(message).hexdigest()}"
+				
 	def sha3_384(self):
 		message = input("Input Pesan : ").encode()
 		return f"\nSHA3-384 : {hashlib.sha3_384(message).hexdigest()}"
-		
-		
-	def sha3_512(self) -> str:
+				
+	def sha3_512(self):
 		message = input("Input Pesan : ").encode()
-		print(f"\nSHA3-512 : {hashlib.sha3_512(message).hexdigest()}")
-		
+		return f"\nSHA3-512 : {hashlib.sha3_512(message).hexdigest()}"		
 		
 	def blake2s(self):
 		message = input("Input Pesan : ").encode()
 		return f"\nBlake2s : {hashlib.blake2s(message).hexdigest()}"
-		
-	
+			
 	def blake2b(self):
 		message = input("Input Pesan : ").encode()
 		return f"\nBlake2b : {hashlib.blake2b(message).hexdigest()}"
-	
-	
+		
 	def md5(self):
 		message = input("Input Pesan : ").encode()
 		return f"\nMD5 : {hashlib.md5(message).hexdigest()}"
 	
 		
 def main():
-	s = Sys_tm(source.Menu())
-	secret = Data()
+	system = SystemSet(Menu())
+	secret = GetHash()
+	algorithms = {
+        1 : secret.sha256,
+        2 : secret.sha384,
+        3 : secret.sha512,
+        4 : secret.sha3_256,
+        5 : secret.sha3_384,
+        6 : secret.sha3_512,
+        7 : secret.blake2s,
+        8 : secret.blake2b,
+        9 : secret.md5
+    }
+    
 	try:
-		c = int(input("Pilih Menu : "))
-		if c == 1:
-			s.clear()
-			print(secret.sha256())
-		elif c == 2:
-			s.clear()
-			print(secret.sha384())
-		elif c == 3:
-			s.clear()
-			print(secret.sha512())
-		elif c == 4:
-			s.clear()
-			secret.sha3_256()
-		elif c == 5:
-			s.clear()
-			print(secret.sha384())
-		elif c == 6:
-			s.clear()
-			secret.sha3_512()
-		elif c == 7:
-			s.clear()
-			print(secret.blake2s())
-		elif c == 8:
-			s.clear()
-			print(secret.blake2b())
-		elif c == 9:
-			s.clear()
-			print(secret.md5())
-		elif c == 0:
-			sys.exit(0)
+		choice = int(input("[+] Pilih Menu : "))
+		if choice in algorithms:
+			system.clear()
+			print(algorithms[choice]())
+		elif choice == 0:
+			exit(0)
 		else:
-			print("\nInput Angka 1-9")
+			print("\nInput Angka 0-9")
 	except ValueError as err:
-		print("\n" + str(err))
+		print("\n" + str(err))		
+
 		
-	
 if __name__ == "__main__" :
-	main()	
+	main()
